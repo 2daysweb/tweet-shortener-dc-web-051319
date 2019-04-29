cea74d82f8aa4a86bcb08cd03f02d7efcdd6e16a
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-29 13:46:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require 'pry'
+
+def dictionary  
+  
+  dictionary =  { 
+    "hello" => 'hi',
+    "to, two, too" => '2',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for, four" => '4',
+    "four" => '4',
+    "for" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    'at' => "@",
+    "and" => "&"
+  }
+  
+end 
+  
+  
+def word_substituter(str_tweet)
+
+tweet_array = []
+tweet_array = str_tweet.split(" ")
+tweet_array.each_with_index do |str,ind|
+  dictionary.each_key do |word|
+    tweet_array[ind].gsub!(word,dictionary[word]) if str.include?(word) && word.length == str.length
+    end
+  end
+ return tweet_array.join(" ")
+end
